2f087607b00c91282cda61ec98d922919ffd143d
Santiago Pizzico <santaigopizzico@hotmail.com>
2022-12-19 22:29:35 -0300

    abm pelis

--- app.py
+++ app.py
@@ -76,3 +76,86 @@ def getPeliculasByPortada():
         return jsonify("Peliculas con portada no encontradas")
     else:
         return jsonify(peliculasByPortada)
+
+#ABM peliculas
+@app.route("/peliculas")
+def getPeliculas():
+    #Obteneniendo JSONs
+    peliculas = fc.obtenerPeliculas()
+    return jsonify(peliculas)
+
+@app.route("/peliculas/create", methods=['POST'])
+def createPelicula():
+    #Obteneniendo JSONs
+    peliculas = fc.obtenerPeliculas()
+    nuevaPelicula = request.get_json()
+    id= fc.nuevaIdPeliculas()
+
+    nuevaPelicula["id"] = id
+    peliculas.append(nuevaPelicula)
+
+    #Actualizando JSONs
+    with open('jsons/peliculas.json', 'w') as archivoJson:
+        json.dump(peliculas, archivoJson, indent=4)
+
+    return 'Pelicula registrada correctamente.'
+
+@app.route("/peliculas/save/", methods=['PUT'])
+def savePelicula():
+    #Obteneniendo JSONs
+    peliculas = fc.obtenerPeliculas()
+
+    nuevaPeliculaDicccionario = request.get_json()
+
+    for pelicula in peliculas:
+        if pelicula["id"] == nuevaPeliculaDicccionario["id"]:
+            if nuevaPeliculaDicccionario["titulo"] != '':
+                pelicula["titulo"] = nuevaPeliculaDicccionario["titulo"]
+            elif nuevaPeliculaDicccionario["ano"] != '':
+                pelicula["ano"] = nuevaPeliculaDicccionario["ano"]
+            elif nuevaPeliculaDicccionario["idDirector"] != '':
+                pelicula["idDirector"] = nuevaPeliculaDicccionario["idDirector"]
+            elif nuevaPeliculaDicccionario["idGenero"] != '':
+                pelicula["idGenero"] = nuevaPeliculaDicccionario["idGenero"]
+            elif nuevaPeliculaDicccionario["sinopsis"] != '':
+                pelicula["sinopsis"] = nuevaPeliculaDicccionario["sinopsis"]
+            elif nuevaPeliculaDicccionario["imagen"] != 'no cargado':
+                pelicula["imagen"] = nuevaPeliculaDicccionario["imagen"]
+
+    #Actualizando JSONs
+    with open('jsons/peliculas.json', 'w') as archivoJson:
+        json.dump(peliculas, archivoJson, indent=4)
+
+    return 'Pelicula exitosamente modificada'
+
+@app.route("/peliculas/delete/<id>/idUsuario/<idUsuario>", methods=['DELETE'])
+def deletePelicula(id, idUsuario):
+    #Obteneniendo JSONs
+    peliculas = fc.obtenerPeliculas()
+    comentarios = fc.obtenerComentarios()
+    comentariosOtrosUsuarios = False
+    valor = {}
+
+    for pelicula in peliculas:
+        if pelicula["id"] == id:
+            for comentarioRecorrido in pelicula["idComentarios"]:
+                for comentario in comentarios:
+                    if comentarioRecorrido == comentario["id"]  and comentario["idUsuario"] != idUsuario:
+                        comentariosOtrosUsuarios = True
+        if comentariosOtrosUsuarios == False and pelicula["id"] == id:
+            valor = pelicula 
+            for peliculaIdComentarios in valor["idComentarios"]:
+                for comentario in comentarios:
+                    if comentario['id'] == peliculaIdComentarios:
+                        comentarios.remove(comentario)
+
+    if comentariosOtrosUsuarios == True:
+        return 'Borrado no exitoso, tiene comentarios de otros usuarios o no se pudo eliminar correctamente'
+    else:
+        peliculas.remove(valor)
+        #Actualizando JSONs
+        with open('jsons/peliculas.json', 'w') as archivoJson:
+            json.dump(peliculas, archivoJson, indent=4)
+        with open('jsons/comentarios.json', 'w') as archivoJson:
+            json.dump(comentarios, archivoJson, indent=4)
+        return 'Borrado exitoso'
